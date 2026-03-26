236cc501e255aa52e8d1815f526ffd9b48621f91
Leticia Alves <letxizalves@gmail.com>
2026-03-26 12:31:11 -0400

    feat: modificando html, css e js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-const sound = document.getElementById('netflix-sound');
 const button = document.querySelector('.enter-btn');
 const app = document.querySelector('.app');
 const splash = document.querySelector('.splash');
-const transition = document.querySelector('.transition');
-
-sound.load();
 
 button.addEventListener('click', () => {
 
@@ -12,30 +8,6 @@ button.addEventListener('click', () => {
 
     setTimeout(() => {
         splash.style.display = 'none';
-
-        // 🎬 mostra transição
-        transition.classList.remove('hidden');
-        transition.classList.add('active');
-
-        // 🔊 som
-        sound.currentTime = 0;
-        sound.play();
-
-        // 🔥 AQUI ESTÁ O SEGREDO (ativa o zoom)
-        setTimeout(() => {
-            transition.classList.add('zoom');
-        }, 200);
-
-        // ⏳ depois some
-        setTimeout(() => {
-            transition.style.opacity = '0';
-
-            setTimeout(() => {
-                transition.style.display = 'none';
-                app.classList.remove('hidden');
-            }, 800);
-
-        }, 1500);
-
-    }, 800);
+        app.classList.remove('hidden');
+    }, 200);
 });
